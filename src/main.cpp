bd8a698609bfda5cfe7c6c3c7b8390f0a13333c7
lune <yagigami.goatgod@gmail.com>
2025-09-23 09:14:08 +0200

    test program with a few instances

--- src/main.cpp
+++ src/main.cpp
@@ -8,5 +8,12 @@ int main()
 	multimedia example("example"s, "path/to/example.type"s);
 	example.display(std::cout);
 	std::cout << '\n';
+	multimedia *ex1 = new multimedia("ex1"s, "path1"s);
+	multimedia *ex2 = new multimedia("ex2"s, "path2"s);
+	ex1->set_path("path1.1"s);
+	ex2->display(std::cout);
+	std::cout << '\n';
+	ex1->display(std::cout);
+	std::cout << '\n';
 }
 
