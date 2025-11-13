8df38ac06992143af6b94bcd607ba39f416377f6
lune0808 <yagigami.goatgod@gmail.com>
2025-11-13 14:24:16 +0100

    add basic window with 3 buttons and text area

---
+++ src/pack/SetTopBox.java
@@ -0,0 +1,42 @@
+package pack;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+
+public class SetTopBox extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	
+	JTextArea text;
+	JButton[] button;
+
+	public static void main(String[] args) {
+		new SetTopBox();
+	}
+	
+	private SetTopBox() {
+		text = new JTextArea(10, 4);
+		button = new JButton[3];
+		button[0] = new JButton("First");
+		button[1] = new JButton("Second");
+		button[2] = new JButton("Third");
+		
+		button[0].addActionListener(e -> text.append("First button clicked."));
+		button[1].addActionListener(e -> text.append("Second button clicked."));
+		button[2].addActionListener(e -> text.append("Third button clicked."));
+		
+		add(text, BorderLayout.SOUTH);
+		add(button[0], BorderLayout.WEST);
+		add(button[1], BorderLayout.NORTH);
+		add(button[2], BorderLayout.EAST);
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("SetTopBox");
+		pack();
+		setVisible(true);
+	}
+
+}
