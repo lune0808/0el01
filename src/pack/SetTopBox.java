bf8b1b7f462fb82e18cc611724b7ced456a7c7fc
lune0808 <yagigami.goatgod@gmail.com>
2025-11-13 15:11:33 +0100

    add menu toolbar and reuse button actions

--- src/pack/SetTopBox.java
+++ src/pack/SetTopBox.java
@@ -1,48 +1,130 @@
 package pack;
 
 import java.awt.BorderLayout;
-
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JScrollBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
+/**
+ * @brief the main class of the SetTopBox package
+ * 
+ * This class is the entry point into the Java side
+ * of the project.
+ */
 public class SetTopBox extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	
 	JTextArea text;
 	JScrollPane textScroll;
-	JButton[] button;
+	JMenuBar menuBar;
+	Action firstAction;
+	Action quitAction;
+	Action thirdAction;
 
+	/**
+	 * Entry point of the program
+	 * 
+	 * @param args ignored
+	 */
 	public static void main(String[] args) {
 		new SetTopBox();
 	}
 	
+	/**
+	 * Spawns a window with UI
+	 */
 	private SetTopBox() {
-		text = new JTextArea(10, 4);
+		text = new JTextArea(40, 80);
 		textScroll = new JScrollPane(text);
-
-		button = new JButton[3];
-		button[0] = new JButton("First");
-		button[1] = new JButton("Quit");
-		button[2] = new JButton("Third");
+		menuBar = new JMenuBar();
+		firstAction = new FirstAction();
+		quitAction = new QuitAction();
+		thirdAction = new ThirdAction();
+		
+		JButton firstButton = new JButton(firstAction);
+		JButton quitButton = new JButton(quitAction);
+		JButton thirdButton = new JButton(thirdAction);
+		JMenu menu = new JMenu("Menu");
+		menu.add(new JMenuItem(firstAction));
+		menu.add(new JMenuItem(quitAction));
+		menu.add(new JMenuItem(thirdAction));
 		
 		text.setLineWrap(true);
-		button[0].addActionListener(e -> text.append("First button clicked."));
-		button[1].addActionListener(e -> System.exit(0));
-		button[2].addActionListener(e -> text.append("Third button clicked."));
 		
-		add(textScroll, BorderLayout.SOUTH);
-		add(button[0], BorderLayout.WEST);
-		add(button[1], BorderLayout.NORTH);
-		add(button[2], BorderLayout.EAST);
+		add(textScroll, BorderLayout.CENTER);
+		add(firstButton, BorderLayout.WEST);
+		add(quitButton, BorderLayout.SOUTH);
+		add(thirdButton, BorderLayout.EAST);
+		menuBar.add(menu);
+		add(menuBar, BorderLayout.NORTH);
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle("SetTopBox");
 		pack();
 		setVisible(true);
 	}
+	
+	/**
+	 * Out-of-line definition for the first action
+	 */
+	private class FirstAction extends AbstractAction {
+
+		private static final long serialVersionUID = 1L;
+
+		public FirstAction() {
+			super("First");
+		}
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			text.append("First button pressed.");
+		}
+		
+	}
+	
+	/**
+	 * Out-of-line definition for the first action
+	 */
+	private class QuitAction extends AbstractAction {
+
+		private static final long serialVersionUID = 1L;
+
+		public QuitAction() {
+			super("Quit");
+		}
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			System.exit(0);
+		}
+		
+	}
+	
+	/**
+	 * Out-of-line definition for the third action
+	 */
+	private class ThirdAction extends AbstractAction {
+
+		private static final long serialVersionUID = 1L;
+
+		public ThirdAction() {
+			super("Third");
+		}
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			text.append("Third button pressed.");
+		}
+		
+	}
 
 }
+
